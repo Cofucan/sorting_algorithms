a4fc612a917cee5c9cadd9199358659b040de76c
cofucan <coolucheofia@gmail.com>
2023-06-24 10:46:05 +0100

    wip: Insertion sort algorithm

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,22 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Sorting Algorithm
+ * @array: The array to be sorted.
+ * @size: The size of the array to be sorted.
+ *
+ * The bubble-sort algorithm for sorting an array of integers.
+ *
+ * Return: Nothing.
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+    listint_t *head, *foo, *bar;
+
+    head = *list;
+    foo = head;
+    bar = head->next;
+
+    if (foo->n > bar->n)
+}
