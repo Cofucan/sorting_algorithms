7e4f1a85c4e5c824dd825c60d6d44844dbfac671
cofucan <coolucheofia@gmail.com>
2023-06-24 05:21:30 +0100

    style: Betty compliant

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,35 +12,34 @@
 
 void bubble_sort(int *array, size_t size)
 {
-    int *left, *right, temp;
-    int swap_occurred;
-    size_t index;
+	int *left, *right, temp;
+	int swap_occurred;
+	size_t index;
 
-    print_array(array, size);
+	print_array(array, size);
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    do
-    {
-        index = 1;
-        left = array;
-        right = array + index;
-        swap_occurred = false;
+	do {
+		index = 1;
+		left = array;
+		right = array + index;
+		swap_occurred = false;
 
-        while (index < size)
-        {
-            if (*left > *right)
-            {
-                temp = *left;
-                *left = *right;
-                *right = temp;
-                swap_occurred = true;
-                print_array(array, size);
-            }
-            index++;
-            left = right;
-            right = array + index;
-        }
-    } while (swap_occurred == true);
+		while (index < size)
+		{
+			if (*left > *right)
+			{
+				temp = *left;
+				*left = *right;
+				*right = temp;
+				swap_occurred = true;
+				print_array(array, size);
+			}
+			index++;
+			left = right;
+			right = array + index;
+		}
+	} while (swap_occurred == true);
 }
