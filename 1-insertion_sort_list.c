ed1816d36ebddd86ac13f2365809e0596ca1d9fb
cofucan <coolucheofia@gmail.com>
2023-06-26 04:06:26 +0100

    docs: Add comments to code

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -13,9 +13,11 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *to_be_sorted, *unsorted;
 
+	/* Check if the list is empty or has only one element */
 	if (!list || !(*list) || !(*list)->next)
 		return;
 
+	/* Start sorting from the second element in the list */
 	unsorted = (*list)->next;
 
 	while (unsorted)
@@ -23,6 +25,7 @@ void insertion_sort_list(listint_t **list)
 		to_be_sorted = unsorted;
 		unsorted = unsorted->next;
 
+		/* Iterate backwards through the sorted portion of the list */
 		while (to_be_sorted->prev && (to_be_sorted->prev->n > to_be_sorted->n))
 		{
 			/* Join the previous node with the next node */
@@ -30,9 +33,11 @@ void insertion_sort_list(listint_t **list)
 			if (to_be_sorted->next)
 				to_be_sorted->next->prev = to_be_sorted->prev;
 
+			/* Move the to_be_sorted node one position to the left */
 			to_be_sorted->next = to_be_sorted->prev;
 			to_be_sorted->prev = to_be_sorted->prev->prev;
 
+			/* Update the links of the adjacent nodes */
 			to_be_sorted->next->prev = to_be_sorted;
 
 			if (to_be_sorted->prev)
@@ -40,6 +45,7 @@ void insertion_sort_list(listint_t **list)
 			else
 				*list = to_be_sorted;
 
+			/* Print the current state of the list */
 			print_list(*list);
 		}
 	}
