c730e5869870c5e6bcbb031f07901237d5c41b54
cofucan <coolucheofia@gmail.com>
2023-06-26 02:12:37 +0100

    feat: Insertion sort algorithm, only printing when the element has been placed in correct position

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -12,19 +12,38 @@
 
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *head, *foo, *bar;
+    listint_t *head, *foo, *bar, *unsorted;
 
     head = *list;
-    foo = head;
-    bar = head->next;
+    unsorted = head;
 
-    if (foo->n > bar->n)
+    while (unsorted->next)
     {
-        bar->prev->next = bar->next;
-        bar->next->prev = bar->prev;
-        bar->prev = foo->prev;
-        bar->next = foo;
-        foo->prev->next = bar;
-        foo->prev = bar;
+        unsorted = unsorted->next;
+        foo = unsorted->prev;
+
+        while (foo)
+        {
+            if ((foo->n > unsorted->n) && (foo->prev == NULL || foo->prev->n <= unsorted->n))
+                break;
+            foo = foo->prev;
+        }
+
+        if (foo)
+        {
+            bar = unsorted;
+            unsorted = unsorted->prev;
+            bar->prev->next = bar->next;
+            if (bar->next)
+                bar->next->prev = bar->prev;
+            bar->prev = foo->prev;
+            bar->next = foo;
+            if (foo->prev)
+                foo->prev->next = bar;
+            else
+                head = bar;
+            foo->prev = bar;
+            print_list(head);
+        }
     }
 }
