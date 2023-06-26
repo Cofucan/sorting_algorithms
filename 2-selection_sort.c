ef5a81a90bb36e8f57eded88c724f0885fdce8e5
Uche Ofia <coolucheofia@gmail.com>
2023-06-26 21:51:33 +0100

    feat:  Selection sort algorithm completed

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,11 +1,11 @@
 #include "sort.h"
 
 /**
- * bubble_sort - Sorting Algorithm
+ * selection_sort - Sorting Algorithm
  * @array: The array to be sorted.
  * @size: The size of the array to be sorted.
  *
- * The bubble-sort algorithm for sorting an array of integers.
+ * A selection-sort algorithm for sorting an array of integers.
  *
  * Return: Nothing.
  */
@@ -13,23 +13,20 @@
 void selection_sort(int *array, size_t size)
 {
 	size_t sort_position, min_position, cursor;
-	int min_number, hold;
+	int hold;
 
 	sort_position = 0;
-	min_position = 1;
 
 	while (sort_position < size - 1)
 	{
-		cursor = sort_position;
-		cursor++;
-		min_number = array[cursor];
+		cursor = sort_position + 1;
+		min_position = cursor;
 
 		while (cursor < size)
 		{
-			if (array[cursor] < min_number)
+			if (array[cursor] < array[min_position])
 			{
 				min_position = cursor;
-				min_number = array[cursor];
 			}
 			cursor++;
 		}
