585f51aaacaddde3e2765edf8cee1fb66ff9fbe9
cofucan <coolucheofia@gmail.com>
2023-06-26 04:30:09 +0100

    wip: Selection sort algorithm

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -12,23 +12,29 @@
 
 void selection_sort(int *array, size_t size)
 {
-   size_t sort_position, min_position, cursor;
-   int hold;
+	size_t sort_position, min_position, cursor;
+	int hold;
 
-   sort_position = cursor = 0;
-   min_position = 1;
+	sort_position = 0;
+	min_position = 1;
 
-   while (sort_position < size - 1)
-   {
-        while (cursor < size)
-        {
-            cursor++;
-            if (array[cursor] < array[sort_position])
-            {
-                min_position = cursor;
-            }
-        }
-        if (array[min_position] < array[sort_position])
+	while (sort_position < size - 1)
+	{
+		cursor = sort_position;
+		while (cursor < size)
+		{
+			cursor++;
+			if (array[cursor] < array[sort_position])
+				min_position = cursor;
+		}
 
-   }
+		if (array[min_position] < array[sort_position])
+		{
+			hold = array[min_position];
+			array[min_position] = array[sort_position];
+			array[sort_position] = hold;
+			print_array(array, size);
+		}
+		sort_position++;
+	}
 }
