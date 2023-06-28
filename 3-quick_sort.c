89058ca6b708c7adc97e19ce522fc20dcef3561e
cofucan <coolucheofia@gmail.com>
2023-06-28 04:29:12 +0100

    docs: Update function descriptions

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,23 +1,35 @@
 #include "sort.h"
 
-void quick_sort_recursion(int *array, size_t size, int *o_array, size_t o_size);
+void quick_sort_recurse(int *array, size_t size, int *o_array, size_t o_size);
 
 /**
- * selection_sort - Sorting Algorithm
+ * quick_sort - Sorting Algorithm
  * @array: The array to be sorted.
  * @size: The size of the array to be sorted.
  *
- * A selection-sort algorithm for sorting an array of integers.
+ * A quick-sort algorithm for sorting an array of integers.
  *
  * Return: Nothing.
  */
 
 void quick_sort(int *array, size_t size)
 {
-	quick_sort_recursion(array, size, array, size);
+	quick_sort_recurse(array, size, array, size);
 }
 
-void quick_sort_recursion(int *array, size_t size, int *o_array, size_t o_size)
+/**
+ * quick_sort_recurse - Sorting Algorithm
+ * @array: The array portion to be sorted in the recursive call.
+ * @size: The size of the array portion to be sorted in the recursive call.
+ * @o_array: Pointer to the start of original array.
+ * @o_size: Size of original array.
+ *
+ * A quick-sort algorithm for sorting an array of integers using recursion.
+ *
+ * Return: Nothing.
+ */
+
+void quick_sort_recurse(int *array, size_t size, int *o_array, size_t o_size)
 {
 	size_t cursor_idx, pivot_idx, swap_idx;
 	int hold, left_size, right_size;
@@ -48,6 +60,6 @@ void quick_sort_recursion(int *array, size_t size, int *o_array, size_t o_size)
 	left_size = swap_idx;
 	right_size = size - swap_idx - 1;
 
-	quick_sort_recursion(array, left_size, o_array, o_size);
-	quick_sort_recursion(array + swap_idx + 1, right_size, o_array, o_size);
+	quick_sort_recurse(array, left_size, o_array, o_size);
+	quick_sort_recurse(array + swap_idx + 1, right_size, o_array, o_size);
 }
